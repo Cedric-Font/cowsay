d57b56873207596ea93db3a0ca43290591051b2e
Cedric-Font <font.cedric38@gmail.com>
2023-10-02 12:23:20 +0200

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log(user.name)
 let cowsay = require('cowsay')
 
 console.log(cowsay.say({
-    text : "Hello my name is " + user.name "",
+    text : "Hello my name is " + user.name + " from " + user.campus,
     e : "oO",
     T : "U "
 }));
